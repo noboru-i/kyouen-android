44feccec8d1def249003cc79a00c341bd1208285
noboru-i <ishikura.noboru@gmail.com>
2012-10-05 00:09:17 +0900

    起動時に、常にGCMの登録IDを送信するように修正

--- src/hm/orz/chaos114/android/tumekyouen/TitleActivity.java
+++ src/hm/orz/chaos114/android/tumekyouen/TitleActivity.java
@@ -287,39 +287,30 @@ public class TitleActivity extends FragmentActivity {
 			// Automatically registers application on startup.
 			GCMRegistrar.register(this, SERVER_ID);
 		} else {
-			// Device is already registered on GCM, check server.
-			if (GCMRegistrar.isRegisteredOnServer(this)) {
-				// Skips registration.
-				Log.i("kyouen", "gcm already registered.");
-			} else {
-				// Try to register again, but not in the UI thread.
-				// It's also necessary to cancel the thread onDestroy(),
-				// hence the use of AsyncTask instead of a raw thread.
-				final Context context = this;
-				AsyncTask<Void, Void, Void> registerTask = new AsyncTask<Void, Void, Void>() {
+			final Context context = this;
+			AsyncTask<Void, Void, Void> registerTask = new AsyncTask<Void, Void, Void>() {
 
-					@Override
-					protected Void doInBackground(Void... params) {
-						boolean registered = ServerUtil.regist(context, regId);
-						// At this point all attempts to register with the app
-						// server failed, so we need to unregister the device
-						// from GCM - the app will try to register again when
-						// it is restarted. Note that GCM will send an
-						// unregistered callback upon completion, but
-						// GCMIntentService.onUnregistered() will ignore it.
-						if (!registered) {
-							GCMRegistrar.unregister(context);
-						}
-						return null;
+				@Override
+				protected Void doInBackground(Void... params) {
+					boolean registered = ServerUtil.regist(context, regId);
+					// At this point all attempts to register with the app
+					// server failed, so we need to unregister the device
+					// from GCM - the app will try to register again when
+					// it is restarted. Note that GCM will send an
+					// unregistered callback upon completion, but
+					// GCMIntentService.onUnregistered() will ignore it.
+					if (!registered) {
+						GCMRegistrar.unregister(context);
 					}
+					return null;
+				}
 
-					@Override
-					protected void onPostExecute(Void result) {
-					}
+				@Override
+				protected void onPostExecute(Void result) {
+				}
 
-				};
-				registerTask.execute(null, null, null);
-			}
+			};
+			registerTask.execute(null, null, null);
 		}
 	}
 
