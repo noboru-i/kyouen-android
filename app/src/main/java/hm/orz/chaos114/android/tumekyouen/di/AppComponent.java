44ce64ba942d8300f3fbbc20ddea804a7df55f6b
noboru-i <ishikura.noboru@gmail.com>
2018-01-05 01:24:52 +0900

    rename class

--- app/src/main/java/hm/orz/chaos114/android/tumekyouen/di/AppComponent.java
+++ app/src/main/java/hm/orz/chaos114/android/tumekyouen/di/AppComponent.java
@@ -5,7 +5,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 import hm.orz.chaos114.android.tumekyouen.modules.initial.InitialActivity;
 import hm.orz.chaos114.android.tumekyouen.modules.kyouen.KyouenActivity;
-import hm.orz.chaos114.android.tumekyouen.modules.kyouen.TumeKyouenFragment;
+import hm.orz.chaos114.android.tumekyouen.modules.kyouen.TumeKyouenView;
 import hm.orz.chaos114.android.tumekyouen.modules.title.TitleActivity;
 import hm.orz.chaos114.android.tumekyouen.modules.title.TitleActivityViewModel;
 
@@ -18,7 +18,7 @@ public interface AppComponent {
 
     void inject(KyouenActivity activity);
 
-    void inject(TumeKyouenFragment fragment);
+    void inject(TumeKyouenView view);
 
     void inject(TitleActivityViewModel titleActivityViewModel);
 }
