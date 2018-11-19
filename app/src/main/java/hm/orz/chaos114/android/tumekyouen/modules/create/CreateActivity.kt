443f21b121f040d48a77f10afe2490d43ac856ba
noboru-i <ishikura.noboru@gmail.com>
2018-11-19 21:46:47 +0900

    add create activity

---
+++ app/src/main/java/hm/orz/chaos114/android/tumekyouen/modules/create/CreateActivity.kt
@@ -0,0 +1,13 @@
+package hm.orz.chaos114.android.tumekyouen.modules.create
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hm.orz.chaos114.android.tumekyouen.R
+
+class CreateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+    }
+}
