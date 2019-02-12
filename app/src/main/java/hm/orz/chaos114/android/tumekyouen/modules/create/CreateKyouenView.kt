a9f00dc8dbbca6e87bc51f8b5867f1baaa7bf8ca
noboru-i <ishikura.noboru@gmail.com>
2019-02-12 22:41:57 +0900

    refactor

--- app/src/main/java/hm/orz/chaos114/android/tumekyouen/modules/create/CreateKyouenView.kt
+++ app/src/main/java/hm/orz/chaos114/android/tumekyouen/modules/create/CreateKyouenView.kt
@@ -2,20 +2,20 @@ package hm.orz.chaos114.android.tumekyouen.modules.create
 
 import android.content.Context
 import android.util.AttributeSet
-
 import hm.orz.chaos114.android.tumekyouen.R
 import hm.orz.chaos114.android.tumekyouen.model.KyouenData
-import hm.orz.chaos114.android.tumekyouen.model.Point
-import hm.orz.chaos114.android.tumekyouen.model.TumeKyouenModel
 import hm.orz.chaos114.android.tumekyouen.modules.common.KyouenView
 import hm.orz.chaos114.android.tumekyouen.modules.kyouen.StoneButtonView
 import hm.orz.chaos114.android.tumekyouen.util.SoundManager
 import timber.log.Timber
 
-class CreateKyouenView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : KyouenView(context, attrs) {
+class CreateKyouenView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null
+) : KyouenView(context, attrs) {
 
-    private var soundManager: SoundManager? = null
-    private var listener: CreateKyouenViewListener? = null
+    private lateinit var soundManager: SoundManager
+    private lateinit var listener: CreateKyouenViewListener
 
     interface CreateKyouenViewListener {
         fun onKyouen(kyouenData: KyouenData)
@@ -27,17 +27,13 @@ class CreateKyouenView @JvmOverloads constructor(context: Context, attrs: Attrib
         this.listener = listener
     }
 
-    override fun setData(stageModel: TumeKyouenModel) {
-        super.setData(stageModel)
-    }
-
     override fun onClickButton(b: StoneButtonView) {
         val index = buttons.indexOf(b)
 
         val col = index % gameModel.size
         val row = index / gameModel.size
         gameModel.putStone(col, row)
-        soundManager!!.play(R.raw.se_maoudamashii_se_finger01)
+        soundManager.play(R.raw.se_maoudamashii_se_finger01)
 
         applyButtons()
 
@@ -46,10 +42,10 @@ class CreateKyouenView @JvmOverloads constructor(context: Context, attrs: Attrib
             Timber.d("kyouenData = %s", kyouenData)
             if (kyouenData != null) {
                 applyWhiteStones(kyouenData)
-                listener!!.onKyouen(kyouenData)
+                listener.onKyouen(kyouenData)
             }
         }
-        listener!!.onAddStone()
+        listener.onAddStone()
     }
 
     fun popStone() {
